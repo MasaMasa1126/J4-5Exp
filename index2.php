5d65803a5896122e909d8d797cc27391a1ae4b23
Masashi SUGIMOTO <41349871+MasaMasa1126@users.noreply.github.com>
2022-07-01 20:42:04 +0900

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,2 +1,3 @@
 <?php
 $url = parse_url(getenv('postgres://cvluishmocgauq:8ce16aca2b4697d1edb36953598153d2d7a13b5ff7d3c3035ea94490c10b6e2d@ec2-52-204-195-41.compute-1.amazonaws.com:5432/d98c1daaci9p7k'));
+?>
